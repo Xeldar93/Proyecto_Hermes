14f1eba4cb45293cd27860c94baf0c6c6354fcee
Jaime M <bssejaimemartinez@gmail.com>
2024-04-22 22:28:01 +0200

    dashboard

--- hermes-test/src/components/Dashboard.jsx
+++ hermes-test/src/components/Dashboard.jsx
@@ -79,24 +79,24 @@ function Dashboard() {
 
   // Muestra el total de ingresos o gastos
   const mostrarTotal = (items) => {
-  if (items.length > 0) {
-    // Filtrar los elementos del último mes
-    const ultimoMesItems = items.filter(item => {
-      const fechaItem = new Date(item.fecha);
-      const mesActual = new Date().getMonth();
-      const mesItem = fechaItem.getMonth();
-      return mesActual === mesItem;
-    });
-
-    // Calcular la suma de los totales del último mes
-    const sumaTotal = ultimoMesItems.reduce((total, item) => {
-      return total + (item.total_ingresos || item.total_gastos || 0);
-    }, 0);
-
-    return sumaTotal;
-  }
-  return 0;
-};
+    if (items.length > 0) {
+      // Filtrar los elementos del último mes
+      const ultimoMesItems = items.filter(item => {
+        const fechaItem = new Date(item.fecha);
+        const mesActual = new Date().getMonth();
+        const mesItem = fechaItem.getMonth();
+        return mesActual === mesItem;
+      });
+  
+      // Calcular la suma de los totales del último mes
+      const sumaTotal = ultimoMesItems.reduce((total, item) => {
+        return total + (item.total_ingresos || item.total_gastos || 0);
+      }, 0);
+  
+      return sumaTotal;
+    }
+    return 0;
+  };
   
 
   useEffect(() => {
